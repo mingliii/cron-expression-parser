6187383d4658c64833635b5a764db8740100b762
Ming Li <li.ming116@gmail.com>
2021-03-31 21:37:33 +0100

    First commit

--- src/main/java/com/interview/CronExpression.java
+++ src/main/java/com/interview/CronExpression.java
@@ -1,49 +1,52 @@
 package com.interview;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
+
+import static com.interview.CronExpression.FIELD_TYPE.*;
+import static java.lang.String.*;
 
 public class CronExpression {
 
     // days in week - ignore the first one
-    private static final int[] _WEEK = new int[8];
+    private static final int[] _DAYS_OF_WEEK = new int[8];
+    private static final String[] _WEEK_BY_NAME = new String[]{null, "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
     // 12 months - ignore the first one
-    private static final int[] _MONTH = new int[13];
+    private static final int[] _MONTHS = new int[13];
     // 31 days in month - ignore the first one
-    private static final int[] _DAY = new int[32];
+    private static final int[] _DAYS_OF_MONTH = new int[32];
     // 24 hours - ignore the first one
-    private static final int[] _HOUR = new int[24];
+    private static final int[] _HOURS = new int[24];
     // 60 minutes in an hour
-    private static final int[] _MINUTE = new int[60];
+    private static final int[] _MINUTES = new int[60];
 
     static {
-        init(0, _WEEK, _MONTH, _DAY);
-        init(1, _HOUR, _MINUTE);
+        init(1, _DAYS_OF_WEEK, _MONTHS, _DAYS_OF_MONTH);
+        init(0, _HOURS, _MINUTES);
     }
 
-    // Field types
-    private static final int MINUTE = 0;
-    private static final int HOUR = 1;
-    private static final int DAY = 2;
-    private static final int MONTH = 3;
-    private static final int WEEK = 4;
-    private static final int COMMAND = 5;
+    enum FIELD_TYPE {
+        MINUTE,
+        HOUR,
+        DAY_OF_MONTH,
+        MONTH,
+        DAY_OF_WEEK,
+        COMMAND
+    }
 
     // Mapping from field types to value ranges
-    private static final Map<Integer, int[]> VALUES_MAP = new HashMap<>(5) {
+    private static final Map<FIELD_TYPE, int[]> VALUES_MAP = new HashMap<>(5) {
         {
-            put(MINUTE, _MINUTE);
-            put(HOUR, _HOUR);
-            put(DAY, _DAY);
-            put(MONTH, _MONTH);
-            put(WEEK, _WEEK);
+            put(MINUTE, _MINUTES);
+            put(HOUR, _HOURS);
+            put(DAY_OF_MONTH, _DAYS_OF_MONTH);
+            put(MONTH, _MONTHS);
+            put(DAY_OF_WEEK, _DAYS_OF_WEEK);
         }
     };
 
-    private final Map<Integer, int[]> RESULTS = new HashMap<>(6);
-
+    private final Map<FIELD_TYPE, int[]> parsedFields = new HashMap<>(6);
+    private String command;
+    private final String expression;
 
     private static void init(int start, int[]... values) {
         for (int[] value : values) {
@@ -54,113 +57,179 @@ public class CronExpression {
     }
 
     public CronExpression(String expression) {
+        this.expression = expression;
         buildExpression(expression);
     }
 
-    protected void buildExpression(String expression) {
+    private void buildExpression(String expression) {
         String[] fields = expression.split(" ");
         if (fields.length != 6) {
-            String errorMsg = String.format("Invalid expression: %s - %s", expression, "Missing field(s)");
-            throw new InvalidExpressionException(errorMsg);
+            throw new InvalidExpressionException(expression, "Missing field(s)");
         }
 
-        for (int i = 0; i < fields.length; i++) {
-            parse(fields[i], i);
+        for (int type = 0; type < fields.length; type++) {
+            if (type == COMMAND.ordinal()) {
+                command = fields[type];
+                continue;
+            }
+            parse(fields[type], FIELD_TYPE.values()[type]);
         }
-
     }
 
-    private void parse(String field, int fieldType) {
+    private void parse(String field, FIELD_TYPE fieldType) {
+
+        // Match all values given field type
         if (Objects.equals(field, "*")) {
             int[] range = VALUES_MAP.get(fieldType);
             if (fieldType != MINUTE && fieldType != HOUR) { // index needs to start from 1
                 range = Arrays.copyOfRange(VALUES_MAP.get(fieldType), 1, range.length);
             }
 
-            RESULTS.put(fieldType, range);
+            parsedFields.put(fieldType, range);
             return;
         }
 
+        // Match nothing
         if (Objects.equals(field, "?")) {
             return;
         }
 
+        // Match a range
         if (field.contains("-")) {
             parseRange(field, fieldType);
+            return;
         }
 
-        if (field.contains("/") && fieldType != COMMAND) {
+        // Match given initial value and step
+        if (field.contains("/")) {
             parseInterval(field, fieldType);
+            return;
         }
 
+        // Match all listed values
         if (field.contains(",")) {
             parseList(field, fieldType);
+            return;
         }
 
         if (field.contains("L")) {
-            handleLastOperator();
+            return;
         }
 
         if (field.contains("W")) {
-            handleWeekday();
+            return;
+        }
+
+        try {
+            parsedFields.put(fieldType, new int[]{Integer.parseInt(field)});
+        } catch (Exception e) {
+            throw new InvalidExpressionException(expression, fieldErrorMsg(field), e);
         }
     }
 
-
-    private void parseRange(String field, int fieldType) {
+    // Parse filed like 1-5, 2-10
+    private void parseRange(String field, FIELD_TYPE fieldType) {
         final int[] values = VALUES_MAP.get(fieldType);
         String[] startEnd = field.split("-");
         if (startEnd.length != 2) {
-            throw new InvalidExpressionException("The field is not valid");
+            throw new InvalidExpressionException(expression, fieldErrorMsg(field));
         }
 
         try {
             int start = Integer.parseInt(startEnd[0]);
             int end = Integer.parseInt(startEnd[1]);
-            RESULTS.put(fieldType, Arrays.copyOfRange(values, start, end + 1));
+            parsedFields.put(fieldType, Arrays.copyOfRange(values, start, end + 1));
         } catch (Exception e) {
-            throw new InvalidExpressionException("The field is not valid: " + field);
+            throw new InvalidExpressionException(expression, fieldErrorMsg(field));
         }
     }
 
-    private void parseInterval(String field, int fieldType) {
+    // Parse field like */5, 1/10
+    private void parseInterval(String field, FIELD_TYPE fieldType) {
         final int[] values = VALUES_MAP.get(fieldType);
         final String[] startEnd = field.split("/");
         if (startEnd.length != 2) {
-            throw new InvalidExpressionException("The field is not valid: " + field);
+            throw new InvalidExpressionException(expression, fieldErrorMsg(field));
         }
+
         try {
-            if (Objects.equals(startEnd[0], "*")) {
-                int[] intervals = Arrays.stream(values).filter(val -> val % Integer.parseInt(startEnd[1]) == 0).toArray();
-                RESULTS.put(fieldType, intervals);
-                return;
+            int start = Objects.equals(startEnd[0], "*") ? 0 : Integer.parseInt(startEnd[0]);
+            int interval = Integer.parseInt(startEnd[1]);
+            if (start > values[values.length - 1] || interval < 1) {
+                throw new InvalidExpressionException(expression, fieldErrorMsg(field));
             }
 
-            int start = Integer.parseInt(startEnd[0]);
-            int end = Integer.parseInt(startEnd[1]);
-            int[] range = Arrays.copyOfRange(values, start, end + 1);
-            int[] intervals = Arrays.stream(range).filter(val -> val % end == 0).toArray();
-            RESULTS.put(fieldType, intervals);
+            int mod = (fieldType == MINUTE || fieldType == HOUR) ? values.length : values.length - 1;
+            int mark = 0;
+
+            List<Integer> results = new ArrayList<>();
+
+            // [0...59]  1/45 => [1, 46, 31, 16]
+            for (int i = mark; i < values.length; i++) {
+                int val = values[i];
+                if (val % interval == 0) {
+                    mark = (val + start) >= mod ? (val + start - mod) % mod : val + start;
+                    results.add(mark);
+                }
+            }
+            while (!results.contains((mark + interval) % mod)) {
+                mark = (mark + interval) % mod;
+                results.add(mark);
+            }
+
+            parsedFields.put(fieldType, results.stream().mapToInt(i -> i).toArray());
         } catch (Exception e) {
-            throw new InvalidExpressionException("The field is not valid: " + field);
+            throw new InvalidExpressionException(expression, fieldErrorMsg(field), e);
         }
     }
 
-    private void parseList(String field, int fieldType) {
-
+    // Parse field like 1,2,3,4,5
+    private void parseList(String field, FIELD_TYPE fieldType) {
+        try {
+            int[] values = Arrays.stream(field.split(",")).mapToInt(val -> VALUES_MAP.get(fieldType)[Integer.parseInt(val)]).toArray();
+            parsedFields.put(fieldType, values);
+        } catch (Exception e) {
+            throw new InvalidExpressionException(expression, fieldErrorMsg(field), e);
+        }
     }
 
-    private void handleLastOperator() {
-
+    public String describe() {
+        String minutes     = "minute        " + join(" ", Arrays.stream(getMinutes()).mapToObj(String::valueOf).toArray(String[]::new));
+        String hours       = "hour          " + join(" ", Arrays.stream(getHours()).mapToObj(String::valueOf).toArray(String[]::new));
+        String daysOfMonth = "day of month  " + join(" ", Arrays.stream(getDaysOfMonth()).mapToObj(String::valueOf).toArray(String[]::new));
+        String months      = "month         " + join(" ", Arrays.stream(getMonths()).mapToObj(String::valueOf).toArray(String[]::new));
+        String daysOfWeek  = "day of week   " + join(" ", Arrays.stream(getDaysOfWeek()).mapToObj(String::valueOf).toArray(String[]::new));
+        String commandStr  = "command       " + command;
+        return join("\n", List.of(minutes, hours, daysOfMonth, months, daysOfWeek, commandStr));
     }
 
-    private void handleWeekday() {
+    private String fieldErrorMsg(String field) {
+        return format("The field '%s' is not valid", field);
+    }
 
+    public int[] getMinutes() {
+        return parsedFields.get(MINUTE);
+    }
+
+    public int[] getHours() {
+        return parsedFields.get(HOUR);
+    }
+
+    public int[] getDaysOfMonth() {
+        return parsedFields.get(DAY_OF_MONTH);
+    }
+
+    public int[] getMonths() {
+        return parsedFields.get(MONTH);
+    }
+
+    public int[] getDaysOfWeek() {
+        return parsedFields.get(DAY_OF_WEEK);
     }
 
     public static void main(String[] args) {
-        CronExpression cronExpression = new CronExpression("*/15 0 1,15 * 1-5 /usr/bin/find");
-
-        System.out.println(cronExpression);
+        CronExpression cronExpression = new CronExpression("*/45 0 1,15 * 1-5 /usr/bin/find");
+        String output = cronExpression.describe();
+        System.out.println(output);
     }
 }
--- src/main/java/com/interview/InvalidExpressionException.java
+++ src/main/java/com/interview/InvalidExpressionException.java
@@ -1,7 +1,16 @@
 package com.interview;
 
 public class InvalidExpressionException extends RuntimeException{
-    public InvalidExpressionException(String errorMsg) {
-        super(errorMsg);
+
+    public InvalidExpressionException(String expression, String reason, Exception e) {
+        super(errorMsg(expression, reason), e);
+    }
+
+    public InvalidExpressionException(String expression, String reason) {
+        super(errorMsg(expression, reason));
+    }
+
+    private static String errorMsg(String expression, String reason) {
+        return String.format("Invalid expression '%s' - reason: %s", expression, reason);
     }
 }
