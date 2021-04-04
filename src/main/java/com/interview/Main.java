23a85913333fab9939c46d37aeb28032aef7173b
Ming Li <li.ming116@gmail.com>
2021-04-04 21:11:31 +0100

    Update main

--- src/main/java/com/interview/Main.java
+++ src/main/java/com/interview/Main.java
@@ -3,9 +3,20 @@ package com.interview;
 public class Main {
 
     public static void main(String[] args) {
+        String expression;
+        if (args.length == 1) {
+            expression =args[0];
+            System.out.println("Cron expression: " + expression);
+        } else {
+            System.out.println("Please provide one cron expression only!");
+            expression = "*/15 * 1,15 * 1-5 /usr/bin/find";
+            System.out.println("This is an example of cron express: " + expression);
+        }
+
         CronExpressionParser parser = new CronExpressionParser();
-//        CronExpressionResults results = parser.parse("*/15 0/23 1-23 * 3-6 /usr/bin/find");
-        CronExpressionResults results = parser.parse("* * * * * /usr/bin/find");
+//        CronExpressionResults results = parser.parse("*/15 * 1,15 * 1-5 /usr/bin/find");
+        CronExpressionResults results = parser.parse(expression);
+        System.out.println("Output:");
         System.out.println(results);
     }
 }
