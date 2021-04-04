29046fcf607009e824947f6336a0538b0c8d5489
Ming Li <li.ming116@gmail.com>
2021-04-04 21:19:00 +0100

    generate jar

--- scripts/parse.sh
+++ scripts/parse.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# ./parse.sh "*/45 0 1,2,15 * 1-5 /usr/bin/find"
-cd ..
-./mvnw compile exec:java -Dexec.mainClass="com.interview.Main" -Dexec.args="'$*'"
-#
-#java -jar deliveroo-cron.jar "$*"
+#cd ..
+#./mvnw compile exec:java -Dexec.mainClass="com.interview.Main" -Dexec.args="'$*'"
+
+java -jar cron-expression-parser.jar "$*"
